731c26f571363b25334c4a671a6a9a7b696c3e2e
Frank Jung <frankhjung@linux.com>
2022-12-18 20:11:15 +1100

    day 11 - monkey in the middle - part 2

--- Day11/src/Solve.hs
+++ Day11/src/Solve.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
--- {-# LANGUAGE FlexibleContexts  #-}
--- {-# LANGUAGE TupleSections     #-}
 {-|
 
 Module      : Solve
@@ -10,29 +8,37 @@ License     : GPL-3
 
 -}
 
-module Solve ( MonkeyTuple
-             , MonkeyMap
+module Solve ( Id
+             , Input
              , Monkey (..)
+             , MonkeyMap
+             , MonkeyTuple
              , Operation (..)
              , Throw (..)
-             , Input
-             , Id
+             , ThrowMap
              , WorryLevel
-             , parse
-             , parseInput
-             , monkeyParser
+             , addWorry
+             , doOp
+             , worry
+             , worry2
+             , getMonkeyId
              , idParser
              , itemsParser
+             , monkeyParser
              , opParser
-             , doOp
-             , getMonkeyId
-             , updateItems
-             , turn
-             , throw
+             , parse
+             , parseInput
              , round
+             , round2
              , score
+             , score2
              , solve
-             , solve2) where
+             , solve2
+             , throw
+             , throw2
+             , turn
+             , turn2
+             ) where
 
 import           Control.Applicative  ((<|>))
 import           Control.Monad        (void)
@@ -43,10 +49,11 @@ import           Data.Either          (fromRight)
 import           Data.Ix              (range)
 import           Data.List            (foldl', sortOn)
 import           Data.Map             (Map)
-import           Data.Map             as M (elems, fromList, insert, lookup,
-                                            size)
-import           Data.Maybe           (fromJust)
+import           Data.Map             as M (adjust, elems, foldr, fromList,
+                                            insert, size, (!))
 import           Data.Ord             (Down (..))
+import           Data.Sequence        (Seq (..))
+import           Data.Sequence        as S (fromList, (|>))
 import           Data.Text            (Text, pack)
 import           Prelude              hiding (round)
 
@@ -59,41 +66,44 @@ type Id = Int
 -- | Human worry level.
 type WorryLevel = Int
 
--- | Map worry items by monkey identifier.
-type MonkeyTuple = (Id, Monkey)
-
-type MonkeyMap = Map Id Monkey
+-- | Parse input into Monkey and Throw by Monkey Id.
+type MonkeyTuple = (Id, Monkey, Throw)
 
 -- | Monkey is collection of item worries, inspected items and throw rules.
 data Monkey = Monkey {
-                _items  :: [WorryLevel] -- ^ items currently held
-              , _throws :: Int          -- ^ count of items throw
-              , _throw  :: Throw        -- ^ `Throw` action
+                _levels :: Seq WorryLevel -- ^ items currently held
+              , _throws :: Int            -- ^ count of items throw
               } deriving (Show, Eq)
+
+-- | A `Throw` consists of a divisibility function and a new monkey id
+-- based on test outcome.
+data Throw = Throw {
+                _op    :: Operation -- ^ pre-test operation
+              , _div   :: Int       -- ^ modulus test
+              , _true  :: Int       -- ^ monkey to throw to if divisible
+              , _false :: Int       -- ^ monkey to throw to not divisible
+             } deriving (Show, Eq)
 
 -- | Pre-inspection `Operation`.
 data Operation = Sum Int | Prod Int | Square deriving (Show, Eq)
 
--- | A `Throw` consists of a predicate function and an action based on the
--- outcome.
-data Throw = Throw {
-                _op        :: Operation -- ^ pre-test operation
-              , _predicate :: Int       -- ^ modulus test
-              , _true      :: Int       -- ^ monkey to throw to if divisible
-              , _false     :: Int       -- ^ monkey to throw to not divisible
-             } deriving (Show, Eq)
+-- | Store items held by each monkey.
+type MonkeyMap = Map Id Monkey
+
+-- | Store throw rules.
+type ThrowMap = Map Id Throw
 
 -- | == Parsers
 
 monkeyParser :: Parser MonkeyTuple
 monkeyParser = do
-  i <- idParser
-  is <- itemsParser
-  op <- opParser
-  p <- throwPredicate
-  t <- throwTrue
-  f <- throwFalse
-  return (i, Monkey is 0 (Throw op p t f))
+  i <- idParser       -- monkey id
+  w <- itemsParser    -- items by worry value
+  o <- opParser       -- operation
+  p <- throwDiv       -- throw test
+  t <- throwTrue      -- monkey to throw to if true
+  f <- throwFalse     -- monkey to throw to if false
+  return (i, Monkey w 0, Throw o p t f)
 
 idParser :: Parser Int
 idParser = do
@@ -103,19 +113,19 @@ idParser = do
   endOfLine
   return i
 
-itemsParser :: Parser [Int]
+itemsParser :: Parser (Seq Int)
 itemsParser = do
   void $ string "  Starting items: "
-  is <- decimal `sepBy1` string ", "
+  ws <- decimal `sepBy1` string ", "
   endOfLine
-  return is
+  return (S.fromList ws)
 
 opParser :: Parser Operation
 opParser = do
   void $ string "  Operation: new = old "
-  op <- sumOp <|> prodOp <|> squareOp
+  o <- sumOp <|> prodOp <|> squareOp
   endOfLine
-  return op
+  return o
 
 sumOp :: Parser Operation
 sumOp = do
@@ -134,12 +144,12 @@ squareOp = do
   void $ string "* old"
   return Square
 
-throwPredicate :: Parser Int
-throwPredicate = do
+throwDiv :: Parser Int
+throwDiv = do
   void $ string "  Test: divisible by "
-  p <- decimal        -- divisibility value to test for
+  n <- decimal        -- divisibility value to test for
   endOfLine
-  return p
+  return n
 
 throwTrue :: Parser Int
 throwTrue = do
@@ -158,57 +168,19 @@ throwFalse = do
 parseInput :: Text -> Either String [MonkeyTuple]
 parseInput = parseOnly ((monkeyParser `sepBy` endOfLine) <* endOfInput)
 
-parse :: Input -> MonkeyMap
-parse = fromList . fromRight [] . parseInput . pack
-
--- | == Monkey Turn
-
--- | Perform worry operation to give a new worry level.
-doOp :: Operation -> Int -> Int
-doOp (Sum s) w  = (w + s) `div` 3
-doOp (Prod p) w = (w * p) `div` 3
-doOp Square w   = (w * w) `div` 3
-
--- | Monkey to next throw to ...
-getMonkeyId :: WorryLevel -> Throw -> Id
-getMonkeyId wl t = if rem wl (_predicate t) == 0 then _true t else _false t
-
--- | Update Monkey items.
-updateItems :: Id -> Monkey -> MonkeyMap -> MonkeyMap
-updateItems = M.insert
-
--- | Monkey turn to throw all it's items.
---
--- monkey i has no more items to throw, so save monkeys state
-turn :: Id -> Monkey -> MonkeyMap -> MonkeyMap
-turn i (Monkey [] s t) ms = M.insert i (Monkey [] s t) ms
-
---  id worry seen oper pred true false
-turn i (Monkey (w:ws) s t) ms =
-  let
-    s' = s + 1                      -- one more item thrown
-    wl = doOp (_op t) w             -- updated worry level
-    i' = getMonkeyId wl t           -- index to monkey ...
-    m' = fromJust (M.lookup i' ms)  -- monkey to update
-    ws' = _items m' ++ [wl]         -- append worry - change to Data.Sequence
-    ms' = updateItems i' (Monkey ws' (_throws m') (_throw m')) ms
-  in turn i (Monkey ws s' t) ms'
-
--- | A throw lets all monkeys have a turn at throwing their stash of items.
-throw :: MonkeyMap -> MonkeyMap
-throw ms = foldl' turnMonkey ms is
+parse :: Input -> (MonkeyMap, ThrowMap)
+parse = go . fromRight [] . parseInput . pack
   where
-    is = range (0, M.size ms - 1)
-    turnMonkey :: MonkeyMap -> Id -> MonkeyMap
-    turnMonkey mm i = turn i (getMonkey i mm) mm
-    getMonkey :: Id -> MonkeyMap -> Monkey
-    getMonkey i mm = fromJust (M.lookup i mm)
+    go :: [(Id, Monkey, Throw)] -> (MonkeyMap, ThrowMap)
+    go xs = (M.fromList ms, M.fromList ts)
+      where
+        ms = map (\(i,m,_) -> (i,m)) xs -- split monkeys
+        ts = map (\(i,_,t) -> (i,t)) xs -- split throw rules
 
--- | A round performs n throws.
-round :: Int -> MonkeyMap -> MonkeyMap
-round n mm = last $ take (succ n) $ iterate throw mm
+-- | == Monkey Round
+-- A round consists of each monkey in turn throwing items.
 
--- | Score
+-- | Score:
 -- Get score of the two monkey's that have thrown the most.
 score :: MonkeyMap -> Int
 score = product . take 2 . sortOn Down . levels
@@ -216,10 +188,117 @@ score = product . take 2 . sortOn Down . levels
     levels :: MonkeyMap -> [Int]
     levels ls = map _throws (elems ls)
 
+-- | Round:
+-- A round performs n throws.
+round :: Int -> (MonkeyMap, ThrowMap) -> MonkeyMap
+round n (m,t) = last $ take (succ n) $ iterate (turn t) m
+
+-- | Turn:
+-- A throw lets all monkeys have a turn at throwing their stash of items.
+turn :: ThrowMap -> MonkeyMap -> MonkeyMap
+turn ts ms = foldl' turn' ms (range (0, M.size ms - 1))
+  where
+    turn' :: MonkeyMap -> Id -> MonkeyMap
+    turn' mm i = throw ts i (mm ! i) mm
+
+-- | Throw:
+-- Monkey's turn to throw all it's items.
+--
+-- Monkey i has no more items to throw, so save monkeys state
+throw :: ThrowMap -> Id -> Monkey -> MonkeyMap -> MonkeyMap
+throw _ i (Monkey Empty s) ms = M.insert i (Monkey Empty s) ms
+
+-- Monkey has more items to throw ...
+throw ts i (Monkey (w:<|ws) s) ms =
+  let
+    s' = s + 1                              -- one more item thrown
+    ms' = worry ts i w ms                   -- update worry level
+  in throw ts i (Monkey ws s') ms'          -- process next worry level
+
+-- | worry:
+-- Update worry level
+--
+-- Id is money whose throw rules we use.
+-- WorryLevel is current value to inspect.
+-- A updated monkey map is returned with monkey been thrown to.
+-- Part 1 - take 1/3 of worry level
+worry :: ThrowMap -> Id -> WorryLevel -> MonkeyMap -> MonkeyMap
+worry ts i w = M.adjust (addWorry w') i'    -- update monkey catching item
+  where
+    w' = doOp (_op (ts!i)) w `div` 3        -- updated worry level
+    i' = getMonkeyId w' (ts!i)              -- index to monkey been thrown to
+
+-- | Add worry level to end of monkey.
+addWorry :: WorryLevel -> Monkey -> Monkey
+addWorry l (Monkey w s) = Monkey (w |> l) s
+
+-- | Perform worry operation to give a new worry level.
+doOp :: Operation -> WorryLevel -> WorryLevel
+doOp (Sum s) w  = s + w
+doOp (Prod p) w = p * w
+doOp Square w   = w * w
+
+-- | Monkey to next throw to ...
+getMonkeyId :: WorryLevel -> Throw -> Id
+getMonkeyId w t = if w `mod` _div t == 0 then _true t else _false t
+
 -- | Solve - part 1 (58794)
 solve :: Input -> Int
 solve = score . round 20 . parse
 
+-- | == Part 2
+
+-- | Score:
+-- Get score of the two monkey's that have thrown the most.
+score2 :: MonkeyMap -> Integer
+score2 = product . map toInteger . take 2 . sortOn Down . levels
+  where
+    levels :: MonkeyMap -> [Int]
+    levels ls = map _throws (elems ls)
+
+-- | Round2:
+-- A round performs n throws.
+round2 :: Int -> (MonkeyMap, ThrowMap) -> MonkeyMap
+round2 n (m,t) = last $ take (succ n) $ iterate (turn2 t) m
+
+-- | Turn2:
+-- A throw lets all monkeys have a turn at throwing their stash of items.
+turn2 :: ThrowMap -> MonkeyMap -> MonkeyMap
+turn2 ts ms = foldl' turn' ms (range (0, M.size ms - 1))
+  where
+    turn' :: MonkeyMap -> Id -> MonkeyMap
+    turn' mm i = throw2 ts i (mm ! i) mm
+
+-- | Throw2:
+-- Monkey's turn to throw all it's items.
+--
+-- Monkey i has no more items to throw, so save monkeys state
+throw2 :: ThrowMap -> Id -> Monkey -> MonkeyMap -> MonkeyMap
+throw2 _ i (Monkey Empty s) ms = M.insert i (Monkey Empty s) ms
+
+-- Monkey has more items to throw ...
+throw2 ts i (Monkey (w:<|ws) s) ms =
+  let
+    s' = s + 1                              -- one more item thrown
+    ms' = worry2 ts i w ms                  -- update worry level
+  in throw2 ts i (Monkey ws s') ms'         -- process next worry level
+
+-- | worry2:
+-- Update worry level - part 2
+--
+-- Id is money whose throw rules we use.
+-- WorryLevel is current value to inspect.
+-- A updated monkey map is returned with monkey been thrown to.
+worry2 :: ThrowMap -> Id -> WorryLevel -> MonkeyMap -> MonkeyMap
+worry2 ts i w = M.adjust (addWorry w'') i'  -- update monkey catching item
+  where
+    o' = _op (ts!i)                         -- opertion for this monkey
+    w' = doOp o' w                          -- updated worry level
+    i' = getMonkeyId w' (ts!i)              -- index to monkey been thrown to
+    w'' = rem w' (worryAdjust ts)           -- updated worry level
+    worryAdjust :: ThrowMap -> Id           -- worry adjustment value.
+    worryAdjust = M.foldr ((*) . _div) 1    -- product of all divisors
+
 -- | Solve - part 2
-solve2 :: Input -> ()
-solve2 = const ()
+solve2 :: Input -> Integer
+solve2 = score2 . round2 10000 . parse
--- Day11/test/Test.hs
+++ Day11/test/Test.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import           Data.Map   as M (lookup)
-import           Data.Maybe (fromJust)
-import           Prelude    hiding (round)
-import           Solve      (Input, Monkey (..), Operation (..), Throw (..),
-                             parse, round, score, solve, solve2, turn)
-import           Test.Hspec (context, describe, hspec, it, shouldBe)
+import           Data.Map      as M ((!))
+import           Data.Sequence (Seq (..))
+import           Data.Sequence as S (fromList)
+import           Solve         (Input, Monkey (..), Operation (..), Throw (..),
+                                parse, solve, solve2, throw)
+import           Test.Hspec    (context, describe, hspec, it, shouldBe)
 
 testMonkeyData :: Input
 testMonkeyData = concat
@@ -19,36 +19,40 @@ testMonkeyData = concat
              ]
 
 testMonkey0 :: Monkey
-testMonkey0 = Monkey [79,98] 0 (Throw (Prod 19) 23 2 3)
+testMonkey0 = Monkey (S.fromList [79,98]) 0
+
+testThrow0 :: Throw
+testThrow0 = Throw (Prod 19) 23 2 3
 
 testTurn0 :: Monkey
-testTurn0 = Monkey [] 2 (Throw (Prod 19) 23 2 3)
+testTurn0 = Monkey Empty 2
 
 main :: IO ()
 main = hspec $ do
   describe "Day11" $ do
-    context "read monkey and list of items" $ do
+    context "read monkey" $ do
       it "expect Monkey 0" $ do
         let
-          ms = parse testMonkeyData
-        M.lookup 0 ms `shouldBe` Just testMonkey0
+          (ms,_) = parse testMonkeyData
+        ms!0 `shouldBe` testMonkey0
+    context "read throw" $ do
+      it "expect Throw 0" $ do
+        let
+          (_,ts) = parse testMonkeyData
+        ts!0 `shouldBe` testThrow0
     context "monkey 0 turn" $ do
       it "expect Monkeys updated and 0 to be empty" $ do
         contents <- readFile "test.data"
         let
-          ms = parse contents
-          w0 = fromJust (M.lookup 0 ms)
-          ms' = turn 0 w0 ms
-        fromJust (M.lookup 0 ms') `shouldBe` testTurn0
-    context "score after 20 rounds" $ do
-      it "expect score 10605" $ do
-        contents <- readFile "test.data"
-        score (round 20 (parse contents)) `shouldBe` 10605
+          (ms,ts) = parse contents
+          w0 = ms!0
+          ms' = throw ts 0 w0 ms
+        ms'!0 `shouldBe` testTurn0
     context "part 1" $
       it "expect 10605" $ do
         contents <- readFile "test.data"
         solve contents `shouldBe` 10605
     context "part 2" $
-      it "expect unit" $ do
+      it "expect 2713310158" $ do
         contents <- readFile "test.data"
-        solve2 contents `shouldBe` ()
+        solve2 contents `shouldBe` 2713310158
